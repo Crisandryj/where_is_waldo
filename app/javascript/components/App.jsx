7783aa291d29b3dd5b86c50829b8bdc8f78dde75
Crisandry Javier <crisandryj@gmail.com>
2025-01-29 14:58:54 -0500

    Refactor: remove image from css file and add styles to react component to set background img

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import TargetBox from "./TargetBox";
+import backgroundImage from "../../assets/images/wheres_waldo_img.jpeg";
 
 const App = () => {
   const [data, setData] = React.useState([]);
+  const styles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover", // Adjust background size as needed
+    backgroundPosition: "center", // Adjust background position as needed
+    minHeight: "100vh",
+    backgroundRepeat: "no-repeat",
+    // Add more styles as needed
+  };
   React.useEffect(() => {
     fetch("/api/v1/characters")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
   return (
-    //   Characters
-    //   {data.map(({ name }) => (
-    //     <div>{name}</div>
-    //   ))}
-    <div>
+    <div style={styles}>
       <TargetBox />
     </div>
   );
