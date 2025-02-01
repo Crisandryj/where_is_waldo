4bf911caf2fd89e24fe032a76ebc54d05bf88c7b
Crisandry Javier <crisandryj@gmail.com>
2025-02-01 10:41:28 -0500

    Refactor: Move styles to style variable for cleaner code

--- app/javascript/components/TargetBox.jsx
+++ app/javascript/components/TargetBox.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import backgroundImage from "../../assets/images/wheres_waldo_img.jpeg";
 
 function TargetBox() {
   const [isDragging, setIsDragging] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const styles = {
+    borderRadius: "100px",
+    position: "absolute",
+    left: position.x,
+    top: position.y,
+    width: "150px",
+    height: "150px",
+    border: "2px dotted",
+    backgroundColor: "",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: '(width * 3) + "px " + (height * 3) + "px"',
+  };
 
   const handleDragStart = (e) => {
     setIsDragging(true);
@@ -15,8 +29,6 @@ function TargetBox() {
       y: e.clientY - e.target.offsetHeight / 2,
     };
     setPosition(newPosition);
-    console.log("is dragging");
-    console.log(newPosition);
   };
 
   const handleDragEnd = () => {
@@ -29,15 +41,7 @@ function TargetBox() {
       onMouseDown={handleDragStart}
       onMouseMove={handleDrag}
       onMouseUp={handleDragEnd}
-      style={{
-        position: "absolute",
-        left: position.x,
-        top: position.y,
-        width: "100px",
-        height: "100px",
-        border: "2px dotted",
-        backgroundColor: "",
-      }}
+      style={styles}
     >
       Drag me!
     </div>
