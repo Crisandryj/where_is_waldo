fa6680cfb8d4886a9f24da4d86203c41eae32f04
Crisandry Javier <crisandryj@gmail.com>
2025-02-04 09:29:07 -0500

    Style: Restyle app and remove unused code

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+// import { useState, useEffect, useRef } from "react";
 import TargetBox from "./TargetBox";
 import backgroundImage from "../../assets/images/wheres_waldo_img.jpeg";
 
 const App = () => {
   const [data, setData] = React.useState([]);
+
   const styles = {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover", // Adjust background size as needed
-    backgroundPosition: "center", // Adjust background position as needed
+    backgroundPosition: "contain", // Adjust background position as needed
     minHeight: "100vh",
+    maxWidth: "100%",
     backgroundRepeat: "no-repeat",
     // Add more styles as needed
   };
+
   React.useEffect(() => {
     fetch("/api/v1/characters")
       .then((res) => res.json())
