dbd61cc1193e7cbc0306a82f7b1d214170c4989a
Crisandry Javier <crisandryj@gmail.com>
2025-02-16 14:18:18 -0500

    Feat: Make btn picture larger for user to see

--- app/javascript/components/ButtonComponent.jsx
+++ app/javascript/components/ButtonComponent.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useRef } from "react";
 
 function Button(props) {
+  const btnStyle = {
+    width: "50px",
+  };
   return (
     <input
       onClick={props.HandleClick}
       type="image"
       alt="Where's waldo?"
       src={props.text}
+      style={btnStyle}
     />
   );
 }
