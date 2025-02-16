f3ca966038383d5c0179162f998525588afd6d09
Crisandry Javier <crisandryj@gmail.com>
2025-02-16 14:15:38 -0500

    Feat: Add images to buttons in order for user to know what char to search

--- app/javascript/components/ButtonComponent.jsx
+++ app/javascript/components/ButtonComponent.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useRef } from "react";
 
 function Button(props) {
-  return <button onClick={props.HandleClick}>{props.text}</button>;
+  return (
+    <input
+      onClick={props.HandleClick}
+      type="image"
+      alt="Where's waldo?"
+      src={props.text}
+    />
+  );
 }
 export default Button;
