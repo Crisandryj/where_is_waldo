d7acfa90950dc28ecdb2d9f8719e5e1de4c82f75
Crisandry Javier <crisandryj@gmail.com>
2025-03-03 11:06:27 -0500

    Refactor: Change location column to x and y to add coordinates for chars

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_24_191608) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_25_165111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
-    t.float "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "x"
+    t.float "y"
   end
 end
