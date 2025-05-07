31f61e8da0f236ec09cd582cae22a1ecea7096de
Crisandry Javier <crisandryj@gmail.com>
2025-05-07 12:01:17 -0400

    Feat: Add a dropdown component to use for chars and selections

---
+++ app/javascript/components/DropDown.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function Dropdown({ options, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (value) => {
+    setSelectedValue(value);
+    onSelect(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown-button" onClick={toggleDropdown}>
+        {selectedValue || "Select an option"}
+      </button>
+      {isOpen && (
+        <ul className="dropdown-options">
+          {options.map((option) => (
+            <li
+              key={option.value}
+              className="dropdown-option"
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
